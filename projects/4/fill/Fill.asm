97a8457c624c2f72f8bdc0ea48cda67282020c05
Shridhar2602 <shridhar2602@gmail.com>
2024-04-05 22:48:48 +0530

    implement multiply and fill programs in assembly

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,85 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+@256
+D = A
+@rows		// number of rows in screen
+M = D
+
+@32
+D = A
+@cols		// number of registers in each row
+M = D
+
+@kbdOutput	// stores the keyboard output
+M = 0
+
+(MAIN)
+	@i
+	M = 0
+
+	@SCREEN
+	D = A
+	@currScreenAddress	// variable storing the address of the screen register to be written into
+	M = D
+
+	@KBD
+	D = M
+	@kbdOutput
+	M = D
+
+	(LOOPROW)
+		@i
+		D = M
+		@rows
+		D = D - M
+		@MAIN
+		D;JGE		// jump to MAIN if all rows painted
+
+		@i
+		M = M + 1
+
+		@j
+		M = 0
+
+		(LOOPCOL)
+			@j
+			D = M
+			@cols
+			D = D - M
+			@LOOPROW
+			D;JGE		// jump to LOOPROW if row j painted
+
+			@kbdOutput
+			D = M
+
+			@BLACK
+			D;JGT		// goto BLACK is any key pressed
+			
+			@WHITE
+			0;JMP
+
+			(BLACK)
+				@currScreenAddress
+				A = M
+				M = -1			// set to black
+				@CONTINUE
+				0;JMP
+		
+			(WHITE)
+				@currScreenAddress
+				A = M
+				M = 0			// set to white
+
+			(CONTINUE)
+			@j
+			M = M + 1			// update j and the screenAddress
+			@currScreenAddress
+			M = M + 1
+
+			@LOOPCOL
+			0;JMP
+
+	@MAIN
+	0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,58 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Using i variable as counter
+//		@i 
+//		M = 0		// i = 0
+//		@R2
+//		M = 0		// ans = 0
+//	(LOOP)
+//		@i
+//		D = M 		// D = i
+//		@R0
+//		D = D - M 	// D = i - R0
+//		@END
+//		D;JGE		// if D <= 0; END
+//	
+//		@i
+//		M = M + 1	// i += 1
+//	
+//		@R1
+//		D = M
+//		@R2
+//		M = M + D	// ans += R1
+//	
+//		@LOOP		// goto LOOP
+//		0;JMP
+//	
+//	(END)
+//		@END
+//		0;JMP
+//	
+
+// Using R0 itself as counter (Less instructions in loop)
+	@R2
+	M = 0		// ans = 0
+	@R0
+	D = M
+	@END
+	D;JEQ
+(LOOP)
+
+	@R1
+	D = M
+	@R2
+	M = M + D	// ans += R1
+
+	@R0
+	D = M - 1	// R0--
+	M = D
+	@END
+	D;JLE		// if R0 <= 0; END
+
+	@LOOP		// goto LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
